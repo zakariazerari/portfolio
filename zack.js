f67721af3a9a4bbcad3eef5c0977d5e5ecf2cc5d
zakariazerari <zakariazerari042@gmail.com>
2024-12-24 21:57:44 +0100

    filter

--- zack.js
+++ zack.js
@@ -5,3 +5,25 @@ var typed = new Typed(".multiple-text",{
     backDelay:1000,
     loop:true
 })
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+winfow.onscroll = ()=> {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offest = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+        };
+    });
+    /*==================== sticky navbar ====================*/
+    let header =document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
